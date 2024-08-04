5c155a3705536aa1760eee019095a1dd4a322545
André <andrelucasbaldaia@gmail.com>
2024-08-04 17:40:27 -0300

    fix: change to the login and registration button

--- my-app/src/components/homepage/navbar.tsx
+++ my-app/src/components/homepage/navbar.tsx
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 export default function Navbar() {
   return (
@@ -12,11 +13,15 @@ export default function Navbar() {
           <img
             src="/img/mini-logo.png"
             alt="Logo"
-            style={{ height: '50px', marginRight: '16px', marginLeft: '0px' }} // Aumentar o tamanho e ajustar a margem
+            style={{ height: '50px', marginRight: '16px', marginLeft: '0px' }}
           />
           <Box sx={{ flexGrow: 1 }} />
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Cadastre-se</Button>
+          <Link href='/login' style={{ textDecoration: 'none' }}>
+            <Button style={{ color: 'white', borderColor: 'white' }}>Login</Button>
+          </Link>
+          <Link href='/register' style={{ textDecoration: 'none' }}>
+            <Button style={{ color: 'white', borderColor: 'white' }}>Cadastre-se</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
