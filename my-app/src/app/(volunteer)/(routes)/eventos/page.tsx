f3ae41ebf664dc36d3c5b58c680c4045b79724c5
André Baldaia <andrelucasbaldaia@gmail.com>
2024-09-28 17:01:09 -0300

    feat: created volunteer list screen event

--- my-app/src/app/(volunteer)/(routes)/eventos/page.tsx
+++ my-app/src/app/(volunteer)/(routes)/eventos/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import * as React from 'react';
+import { EventsList } from "@/components/volunteer/events/eventsList";
+import BackgroundImage from '@/components/ui/background-image';
 
 export default function Home() {
 
   return (
-    <>
-      <a>Todos os eventos</a>
-    </>
+    <div className="h-screen">
+      <BackgroundImage className="h-full w-full flex items-center justify-center">
+        
+        <EventsList />
+      </BackgroundImage>
+    </div>
   );
 }
---
+++ my-app/src/components/volunteer/events/eventsList.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import * as React from "react";
+import { EventCard } from "@/components/shared/eventCard";
+import FilterComponent from "@/components/shared/filter";
+import SearchComponent from "@/components/shared/search";
+import Link from "next/link";
+
+export function EventsList() {
+  return (
+    <div>
+      <div className=" pt-60 flex justify-end">
+        <SearchComponent />
+      </div>
+      <h1 className="font-bold leading-none tracking-tight text-[#702054] text-[34px]">Eventos cadastrados</h1>
+      <div>
+        <FilterComponent/>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-6">
+      <Link href="/detalhes-do-evento">
+        <EventCard />
+      </Link>
+      <Link href="/detalhes-do-evento">
+        <EventCard />
+      </Link>
+      <Link href="/detalhes-do-evento">
+        <EventCard />
+      </Link>
+      <Link href="/detalhes-do-evento">
+        <EventCard />
+      </Link>
+      </div>
+    </div>
+  );
+}
