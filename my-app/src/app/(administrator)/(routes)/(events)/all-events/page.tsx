a577fd16710ea77f918b79da8ebf4b20ef2a2933
André Baldaia <andrelucasbaldaia@gmail.com>
2024-09-28 17:15:32 -0300

    feat: created administrator list screen event

--- my-app/src/app/(administrator)/(routes)/(events)/all-events/page.tsx
+++ my-app/src/app/(administrator)/(routes)/(events)/all-events/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import * as React from 'react';
+import { EventsList } from "@/components/administrator/events/eventsList";
+import BackgroundImage from '@/components/ui/background-image';
 
 export default function Events() {
 
   return (
-    <>
-        <h1>Todos os eventos</h1>
-    </>
+    <div className="h-screen">
+      <BackgroundImage className="h-full w-full flex items-center justify-center">
+        
+        <EventsList />
+      </BackgroundImage>
+    </div>
   );
 }
