452e4bf9065fe2f10c133d73d9a1a2231d4d20c6
André Baldaia <andrelucasbaldaia@gmail.com>
2024-09-30 22:20:53 -0300

    fix: changed background image of all events page

--- my-app/src/app/(beneficiary)/(routes)/(events)/todos-os-eventos/page.tsx
+++ my-app/src/app/(beneficiary)/(routes)/(events)/todos-os-eventos/page.tsx
@@ -4,10 +4,15 @@ import { EventsList } from '@/components/beneficiary/events/eventsList';
 export default function Events() {
   return (
     <div className="h-screen">
-      <BackgroundImage className="h-full w-full flex items-center justify-center">
-        
+      <div className="h-full w-full flex items-center justify-center"  style={{
+        backgroundImage: "url('/img/backgroundgirls.png')",
+        backgroundSize: "cover", 
+        backgroundPosition: "center", 
+        backgroundAttachment: "fixed", 
+      }}>
+    
         <EventsList />
-      </BackgroundImage>
+      </div>
     </div>
   );
 };
