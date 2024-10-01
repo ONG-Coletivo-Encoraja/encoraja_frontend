a0b5e6b06f0d3cb623cbb40dbe4a490d930b0cc4
julianabaicar <baicarjuliana@gmail.com>
2024-10-01 00:23:09 -0300

    feat: SCRUM-23-Event-edit-screen-by-user: Implemantation forms.

---
+++ my-app/src/components/administrator/events/eventsUpdate.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Events from "@/components/shared/eventUpdate";
+import BackgroundImage from '@/components/ui/background-image';
+
+export default function EventUpdate() {
+
+  return (
+    <>
+    {/* Adicionar total de inscritos */}
+      <BackgroundImage className="h-full w-full flex items-center justify-center">
+      < Events />
+      </BackgroundImage>
+    </>
+  );
+}
