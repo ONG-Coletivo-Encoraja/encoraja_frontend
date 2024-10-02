b52afd320001fe0b359545a01a33b58a07f8c562
Maria Eduarda <silvamariaeduarda92@gmail.com>
2024-10-02 19:05:25 -0300

    fix: removed use client from auth pages

--- my-app/src/app/(auth-routes)/login/page.tsx
+++ my-app/src/app/(auth-routes)/login/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-
 import LoginForm from "../../../components/login/loginform"
 
 
--- my-app/src/app/(auth-routes)/register/page.tsx
+++ my-app/src/app/(auth-routes)/register/page.tsx
@@ -1,14 +1,13 @@
 import RegisterForm from "../../../components/register/registerform";
 
 export default function Login() {
-    return (
-      <div className="flex items-center justify-center min-h-screen"  style={{
-        backgroundImage: "url('/img/backgroundgirls.png')",
-        backgroundSize: "cover", 
-        backgroundPosition: "center", 
-      }}>
-        <RegisterForm />
-      </div>
-      
-    );
-  }
+  return (
+    <div className="flex items-center justify-center min-h-screen" style={{
+      backgroundImage: "url('/img/backgroundgirls.png')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}>
+      <RegisterForm />
+    </div>
+  );
+}
