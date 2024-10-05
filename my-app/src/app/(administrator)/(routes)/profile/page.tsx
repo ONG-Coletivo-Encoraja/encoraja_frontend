d09529db19155c9c32e4f9ace77ac1814e883bef
André Baldaia <andrelucasbaldaia@gmail.com>
2024-10-05 00:36:18 -0300

    fix: corrected fields in the profile editing form

--- my-app/src/app/(administrator)/(routes)/profile/page.tsx
+++ my-app/src/app/(administrator)/(routes)/profile/page.tsx
@@ -5,10 +5,14 @@ import BackgroundImage from '@/components/ui/background-image';
 export default function ProfilePage() {
 
   return (
-    <div className="h-screen">
-      <BackgroundImage className="h-full w-full flex items-center justify-center">
+    <div className="h-screen" style={{
+      backgroundImage: "url('/img/backgroundgirls.png')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}>
+      <div className="h-full w-full flex items-center justify-center">
         <Profile />
-      </BackgroundImage>
+      </div>
     </div>
   );
 }
--- my-app/src/app/(auth-routes)/login/page.tsx
+++ my-app/src/app/(auth-routes)/login/page.tsx
@@ -2,13 +2,13 @@ import LoginForm from "../../../components/login/loginform"
 
 
 export default function Login() {
-    return (
-      <div className="flex items-center justify-center min-h-screen"  style={{
-        backgroundImage: "url('/img/backgroundgirls.png')",
-        backgroundSize: "cover", 
-        backgroundPosition: "center", 
-      }}>
-        <LoginForm/>
-      </div>
-    );
-  }
+  return (
+    <div className="flex items-center justify-center min-h-screen" style={{
+      backgroundImage: "url('/img/backgroundgirls.png')",
+      backgroundSize: "cover",
+      backgroundAttachment: "fixed", 
+    }}>
+      <LoginForm/>
+    </div>
+  );
+}
