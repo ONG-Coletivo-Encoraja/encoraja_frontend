f63a71439788c8c6764970db3eda7086d1c181c6
julianabaicar <baicarjuliana@gmail.com>
2024-11-11 20:20:07 -0300

    fix: SCRUM-329-Corrigir-desing-HomePage-front fix elements.

--- my-app/src/app/(homepage)/page.tsx
+++ my-app/src/app/(homepage)/page.tsx
@@ -7,6 +7,7 @@ import { EventsCard } from '@/components/homepage/events';
 export default function Home() {
   return (
     <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
   <div className="fixed top-0 left-0 w-full z-10">
         <Navbar />
       </div>
@@ -17,43 +18,48 @@ export default function Home() {
           style={{ backgroundImage: "url('/img/homepage1bg.png')" }}
         >
          
-          <div className="text-white font-bold text-5xl text-center max-w-[80%]">
-              <p>NÓS AJUDAMOS</p>
-              <h1>MULHERES</h1> 
-              <p>a romperem ciclos de violência e criar oportunidades de geração de renda e inclusão econômica!</p>
-          </div>
-        
+         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+          <Label style={{ color: 'white', fontSize: '2rem', fontFamily: 'Rosario', marginBottom: '1rem', maxWidth: '80%' }}>Nós ajudamos</Label>
+          <Label style={{ color: 'white', fontSize: '8rem', fontFamily: 'Peace Sans', fontWeight: 'bold', maxWidth: '80%' }}>MULHERES</Label>
+          <Label style={{ color: 'white', fontSize: '2rem', fontFamily: 'Rosario', marginBottom: '1rem', maxWidth: '80%' }}>
+            a romperem ciclos de violência e criar oportunidades de geração de renda e inclusão econômica!
+          </Label>
+        </div>
+
         </div>
 
         <div 
           id="about-us" 
-          className="grid grid-cols-3 gap-4 h-screen items-center bg-no-repeat bg-cover bg-center m-0 p-0"
+          className="w-full min-h-screen bg-no-repeat bg-cover bg-center p-8"
           style={{ backgroundImage: "url('/img/homepage2bg.png')" }}
         >
-
-          <div id='about-us' className="grid grid-cols-3 gap-4 h-screen flex items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-start">
             <AboutUsCard
-            title="Quem somos?"
-            content="Um Coletivo de mulheres que atuam no combate à violência doméstica e abuso infantil. Oferecendo cursos que capacitam mulheres visando atingir a independência financeira (como por exemplo: oficinas de costura, pintura, entre outros)." 
+              icon="img/iconencoraja2.png"
+              title="Quem somos?"
+              content="Um Coletivo de mulheres que atuam no combate à violência doméstica e abuso infantil. Oferecendo cursos que capacitam mulheres visando atingir a independência financeira (como por exemplo: oficinas de costura, pintura, entre outros)." 
             />
             <AboutUsCard
-            title="Nossa Missão?"
-            content="Empoderar mulheres através do acolhimento, capacitação profissional e orientação, promovendo sua autonomia e abrindo caminhos para uma vida digna e independente."
+              icon="img/iconmission.png"
+              title="Nossa Missão?"
+              content="Empoderar mulheres através do acolhimento, capacitação profissional e orientação, promovendo sua autonomia e abrindo caminhos para uma vida digna e independente."
             />
             <AboutUsCard
-            title="Diferencial"
-            content="Com programas de capacitação focados no desenvolvimento financeiro e profissional, oferecemos suporte personalizado para que cada mulher atinja seus objetivos de independência."
+              icon="img/icondifferential.png"
+              title="Diferencial"
+              content="Com programas de capacitação focados no desenvolvimento financeiro e profissional, oferecemos suporte personalizado para que cada mulher atinja seus objetivos de independência."
             />
             <AboutUsCard
-            title="Visão"
-            content="Ser uma referência no apoio ao desenvolvimento pessoal e financeiro de mulheres, inspirando e capacitando para que se tornem agentes de transformação em suas comunidades."
+              icon="img/iconvision.png"
+              title="Visão"
+              content="Ser uma referência no apoio ao desenvolvimento pessoal e financeiro de mulheres, inspirando e capacitando para que se tornem agentes de transformação em suas comunidades."
             />
             <AboutUsCard
-            title="Valores"
-            content="Valorizamos o respeito, a diversidade, a ética e a transparência em todas as nossas ações, promovendo um ambiente de de acolhimento e seguro. Acreditamos no poder de cada mulher de construir um futuro com mais oportunidades e igualdade" 
+              icon="img/iconvalues.png"
+              title="Valores"
+              content="Valorizamos o respeito, a diversidade, a ética e a transparência em todas as nossas ações, promovendo um ambiente de acolhimento e seguro. Acreditamos no poder de cada mulher de construir um futuro com mais oportunidades e igualdade" 
             />
           </div>
-
         </div>
 
 
@@ -64,10 +70,11 @@ export default function Home() {
           >
             <div>
             </div>
-            <div>
-              <Label className="text-[#73225D] text-4xl mb-4 text-center">Sobre as fundadoras</Label>
-              <Label className="text-[#73225D] text-sm text-justify max-w-[80%]">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur ipsam officia accusamus earum ducimus est cupiditate aperiam itaque molestias tempora animi optio laborum nisi recusandae, cumque ut vitae ratione at.
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-0.5 gap-x-0.5 h-auto py-10" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+              <Label className="text-[#73225D] md:text-3xl" style={{ fontSize: '3rem', fontFamily: 'Peace Sans', fontWeight: 'bold', maxWidth: '80%' }}>Sobre as fundadoras</Label>
+              <Label className="text-[#73225D] md:text-3xl" style={{ fontSize: '2rem', fontFamily: 'Rosario', marginBottom: '1rem', maxWidth: '80%' }}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur ipsam officia accusamus earum ducimus est cupiditate aperiam itaque molestias tempora animi optio laborum nisi recusandae, cumque ut vitae ratione at.
               </Label>
             </div>
           </div>
@@ -76,15 +83,17 @@ export default function Home() {
             id="events"
             className="w-full min-h-screen flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center p-8"
             style={{ backgroundImage: "url('/img/homepage4bg.png')" }}
-          >
-            {/* Título da seção de eventos */}
-            <div className="text-center mb-8">
-              <Label className="text-3xl">Projetos que transformam vidas</Label>
-              <Label className="text-xl mt-2">Conheça nossos projetos e faça parte dessa transformação</Label>
+            >
+            <div className="text-center md:mb-8 flex flex-col items-center">
+              <Label className="text-[#F2F2F2] text-3xl font-bold" style={{ fontSize: '3rem', fontFamily: 'Peace Sans' }}>
+                Projetos que transformam vidas
+              </Label>
+              <Label className="text-[#F2F2F2] text-xl mt-2 italic" style={{ fontSize: '2rem', fontFamily: 'Peace Sans' }}>
+                Conheça nossos projetos e faça parte dessa transformação
+              </Label>
             </div>
 
-            {/* Cards de eventos */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-screen-lg">
+            <div className="grid gap-6 mt-6 w-full max-w-5xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <EventsCard 
                 img={<img src="img/events/instituto-ama.jpeg" alt="instituto-ama" />}
                 title="Instituto Ama"
@@ -108,39 +117,45 @@ export default function Home() {
             </div>
           </div>
 
+
           <div 
-            id='partners'
-            className="w-full min-h-screen flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center p-8"
+            id="partners"
+            className="w-full min-h-screen flex flex-col items-center justify-start bg-no-repeat bg-cover bg-center p-8"
             style={{ backgroundImage: "url('/img/homepage5bg.png')" }}
           >
-            <div className='flex flex-col items-center justify-center'>
-              <Label className="text-3xl">Nossos parceiros e apoiadores</Label>
+            <div className="flex flex-col items-center justify-start pt-12"> 
+              <Label className="text-[#F2F2F2] text-3xl font-bold text-center" style={{ fontSize: '2rem', fontFamily: 'Peace Sans', fontWeight: 'bold', marginBottom: '1rem', maxWidth: '80%' }}>Nossos parceiros e apoiadores</Label>
+              
+              <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 mt-6">
+                <img 
+                  src="img/partners/luar-da-baronesa.png" 
+                  className="max-w-full max-h-[150px] object-contain"
+                  alt="Luar da Baronesa"
+                />
+                <img 
+                  src="img/partners/cebrac.png" 
+                  className="max-w-full max-h-[150px] object-contain"
+                  alt="Cebrac"
+                />
+                <img 
+                  src="img/partners/elas-ong.png" 
+                  className="max-w-full max-h-[150px] object-contain"
+                  alt="Elas"
+                />
+                <img 
+                  src="img/partners/mulheres-em-movimento.png" 
+                  className="max-w-full max-h-[150px] object-contain"
+                  alt="Mulheres em movimento"
+                />
+              </div>
             </div>
-            <div className="grid grid-cols-4 gap-4 mt-6">
-              <img 
-                src="img/partners/luar-da-baronesa.png" 
-                className="max-w-full max-h-[150px] object-contain"
-                alt="Luar da Baronesa"
-              />
-              <img 
-                src="img/partners/cebrac.png" 
-                className="max-w-full max-h-[150px] object-contain"
-                alt="Cebrac"
-              />
-              <img 
-                src="img/partners/elas-ong.png" 
-                className="max-w-full max-h-[150px] object-contain"
-                alt="Elas"
-              />
-              <img 
-                src="img/partners/mulheres-em-movimento.png" 
-                className="max-w-full max-h-[150px] object-contain"
-                alt="Mulheres em movimento"
-              />
-            </div>
-            <Label className='m-8 text-3xl'>Contatos</Label>
-          </div>  
-      </div>
+
+            <Label className="text-[#73225D] sm:mt-60 text-3xl" style={{ fontSize: '2rem', fontFamily: 'Peace Sans', fontWeight: 'bold', marginBottom: '1rem', maxWidth: '80%' }}>
+              Contatos
+            </Label>
+          </div>
+
+        </div>
   </>
   );
 }
--- my-app/src/components/homepage/about-us.tsx
+++ my-app/src/components/homepage/about-us.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 import { CardTitle, CardContent } from "@/components/ui/card";
 
-export function AboutUsCard({ title, content }: { title: string, content: any }) {
+export function AboutUsCard({ icon, title, content }: { icon: string, title: string, content: any }) {
   return (
-    <div className="h-[250px] w-[400px] bg-transparent m-0 p-0">
-      <div className="bg-transparent">
-        <CardTitle className="ml-5 text-[#F2F2F2]">
+    <div className="w-[400px] bg-transparent m-0 p-0 text-center">
+    <div className="bg-transparent p-4">
+      {/* Ícone centralizado e com tamanho fixo */}
+      <img src={icon} alt={`${title} icon`} className="w-16 h-16 mx-auto mb-4" />
+
+      <div className="bg-transparent p-4 text-center">
+        <CardTitle className="text-[#F2F2F2] text-xl font-bold mb-2">
           {title}
         </CardTitle>
-        <CardContent className="mt-4 text-[#F2F2F2]">
+        <CardContent className="text-[#F2F2F2] text-base">
           {content}
         </CardContent>
       </div>  
+    </div>
     </div>
   );
 }
